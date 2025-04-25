59ef14c7865ed0b6a9289f862ad633f97e608161
Thien0112 <128010271+Rebalon@users.noreply.github.com>
2025-04-25 20:35:20 +0700

    Update MVC

--- my-react-app/src/Backend/controllers/homeController.js
+++ my-react-app/src/Backend/controllers/homeController.js
@@ -1,21 +1,46 @@
-const { insertNewUser } = require('../services/CRUDService')
+const User = require('../models/userModel');
 
 const registerUser = async (req, res) => {
     const { username, email, displayName, password } = req.body;
 
+    // Check if the required fields are present
     if (!username || !email || !password) {
         return res.status(400).json({ message: 'Missing required fields.' });
     }
 
     try {
-        await insertNewUser(username, email, displayName, password);
-        res.status(201).json({ message: 'User created successfully' });
+        // Insert the new user into the database
+        const userId = await User.insertNewUser(username, email, displayName, password);
+
+        // Respond with success message and user ID
+        res.status(201).json({ message: 'User created successfully', userId });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Server error' });
     }
-}
+};
+
+const loginUser = async (req, res) => {
+    const { userIdentifier, password } = req.body;
+
+    // Check if the required fields are present
+    if (!userIdentifier || !password) {
+        return res.status(400).json({ message: 'Missing required fields.' });
+    }
+
+    try {
+        // Insert the new user into the database
+        const userId = await User.checkUserCredentials(userIdentifier, password);
+
+        // Respond with success message and user ID
+        res.status(201).json({ message: 'Login accept', userId });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
 module.exports = {
-    registerUser
-}
+    registerUser,
+    loginUser
+};
